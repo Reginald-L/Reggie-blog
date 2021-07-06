a05b6b154646544afc6d1f8a58b8f27a364403f6
Reggie <lireginald370@gmail.com>
2021-07-06 21:33:47 +1200

    initial

---
+++ docusaurus.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  title: "Reggie's blog",
+  tagline: 'The tagline of my site',
+  url: 'https://reggiedev.tech/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Reginald-L', 
+  projectName: 'Reggie-blog',
+  themeConfig: {
+    navbar: {
+      title: 'Reggie',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/favicon.ico',
+      },
+      items: [
+        {to: '/', label: 'Blog', position: 'right'},
+        {
+          href: 'https://github.com/Reginald-L',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        blog: {
+            path: './blog',
+            routeBasePath: '/', 
+        },
+        theme: {
+            customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  clientModules: [
+    require.resolve('./src/js/love.min.js'),
+  ],
+};
